88883c6f3fa04be4604390584796b701b27529e1
Masoud <95829809+MasoudMsd@users.noreply.github.com>
2025-03-08 07:42:09 +0100

    run.sh

--- run.sh
+++ run.sh
@@ -1,11 +1,4 @@
-#!/bin/bash
-# First check that Leo is installed.
-if ! command -v leo &> /dev/null
-then
-    echo "leo is not installed."
-    exit
-fi
-# Create a new game.
+
 echo "
 ###############################################################################
 ########                                                               ########
